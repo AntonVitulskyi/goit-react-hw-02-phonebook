7d01652152904c36a7be407bdaa0ca118650e158
AntonVitulskyi <vitulskiyanton@gmail.com>
2022-12-21 14:05:42 +0200

    All tasks done

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,47 @@
-import { Component } from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
+import ContactItem from 'components/ContactItem/ContactItem';
+import styles from '../ContactList/ContactList.module.css';
 
-export default class ContactList extends Component {
-    state = {
-      name: "",
-      quantity: "",
-      price: "",
-    };
+export default function ContactList({
+  contacts,
+  filter,
+  onClickDeleteContact,
+}) {
+  let filteredContacts = contacts.filter(contact => {
+    return contact.name
+      .toLowerCase()
+      .trim()
+      .includes(`${filter}`.toLowerCase().trim());
+  });
 
-    
+  return (
+    <>
+      <h1 className={styles.title}>Contacts</h1>
+      <ul className={styles.list}>
+        {filteredContacts.map(contact => (
+          <ContactItem
+            key={contact.id}
+            id={contact.id}
+            name={contact.name}
+            number={contact.number}
+            onClickDeleteContact={onClickDeleteContact}
+          />
+        ))}
+      </ul>
+    </>
+  );
 }
+
+ContactItem.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      id: PropTypes.string,
+      number: PropTypes.string,
+    })
+  ),
+  filter: PropTypes.string,
+  onClickDeleteContact: PropTypes.func,
+};
